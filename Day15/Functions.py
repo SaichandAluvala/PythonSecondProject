a1ec0d50e4abc75199752fb2cdc8f289d596d2aa
Saichand Aluvala <saichandaluvala12345@gmail.com>
2024-07-08 10:27:21 +0530

    Update Functions.py
    removed unneeded print requests

--- Day15/Functions.py
+++ Day15/Functions.py
@@ -6,16 +6,16 @@ function_name()
 
 def function_name(a,b):
     print(a+b)
-print(function_name(1,2))
+function_name(1,2)
 def add(a,b):
     print(a+b)
-print(add(2,3))
-print(add(4,5))
+add(2,3)
+add(4,5)
 
 #retun date into function name
 def sub(a,b):
     return a-b
-print(sub(3,2))
+sub(3,2)
 
 z=sub(5,2)
 print(z)
